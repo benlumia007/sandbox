f1243c727306991319653b291e32f89d682b582a
Benjamin Lu <benlumia007@gmail.com>
2019-02-05 16:30:29 -0800

    updated with new box

--- provision/setup.sh
+++ provision/setup.sh
@@ -7,104 +7,52 @@ noroot() {
 }
 
 # MySQL Configuration
-if [[ ! -f /home/vagrant/.my.cnf ]]; then
-    echo "Copying /srv/config/mysql/.my.cnf     /home/vagrant/.my.cnf"
-    cp "/srv/config/mysql/.my.cnf" "/home/vagrant/.my.cnf"
-
-    chmod 0755 /home/vagrant/.my.cnf
-
-    mysql -u root -proot -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH 'mysql_native_password' BY 'root';"
-    mysql -u root -proot -e "FLUSH PRIVILEGES;"
-
-    echo "create wordpress user"
-    mysql -u root -proot -e "CREATE USER 'wp'@'localhost' IDENTIFIED BY 'wp';"
-    mysql -u root -proot -e "GRANT USAGE ON *.* TO 'wp'@'localhost';"
-    mysql -u root -proot -e "FLUSH PRIVILEGES;"
-else
-    echo ".my.cnf is already been configured"
-    echo "user wp has been created"
-fi
-
-if [[ -f /etc/mysql/mysql.cnf ]]; then
-    echo "Copying /srv/config/mysql/mysql.cnf   /etc/mysql/mysql.cnf"
-    cp -rf "/srv/config/mysql/mysql.cnf" "/etc/mysql/mysql.cnf"
-    echo "Restarting MySQL Server"
+if [[ ! -f "/srv/log/mysql/slow.log" ]]; then
+    echo "copy /srv/config/mysql/my.cnf   /etc/mysql/my.cnf"
+    cp "/srv/config/mysql/my.cnf" "/etc/mysql/my.cnf"
+    echo "restarting mysql server"
     service mysql restart
-    mkdir -p /srv/log/mysql
-    touch /srv/log/mysql/slow.log
+    mkdir -p "/srv/log/mysql"
+    touch "/srv/log/mysql/slow.log"
 else
-    echo "mysql.cnf has been configured."
+    echo "my.cnf has been copied and configured."
 fi
 
-echo "PHP Configuration"
-if [[ ! -f /etc/php/7.2/mods-available/php-custom.ini ]]; then
-    echo "Copying /srv/config/php/php-custom.ini   /etc/php/7.2/mods-available/php-custom.ini"
+if [[ ! -f "/etc/php/7.2/mods-available/php-custom.ini" ]]; then
+    echo "copy /srv/config/php/php-custom.ini   /etc/php/7.2/mods-available/php-custom.ini"
     cp "/srv/config/php/php-custom.ini" "/etc/php/7.2/mods-available/php-custom.ini"
     phpenmod php-custom
-    mkdir -p /srv/log/php
-    touch /srv/log/php/php_errors.log
-    echo "Restarting Apache Server"
+    mkdir -p "/srv/log/php"
+    touch "/srv/log/php/php_errors.log"
+    echo "restarting apache server"
     service apache2 restart
 else
     echo "php-custom.ini has been configured and enabled."
 fi
 
-# Configure Mailcatcher
-if [[ ! -f /lib/systemd/system/mailcatcher.service ]]; then
-    echo "Copying /srv/config/mailcatcher/mailcatcher.service   /lib/systemd/system/mailcatcher.service"
+if [[ ! -f "/lib/systemd/system/mailcatcher.service" ]]; then
+    echo "copy /srv/config/mailcatcher/mailcatcher.service   /lib/systemd/system/mailcatcher.service"
     cp "/srv/config/mailcatcher/mailcatcher.service" "/lib/systemd/system/mailcatcher.service"
-    echo "Enable Mailcatcher at Startup"
+    echo "starting mailcatcher at startup"
     systemctl enable mailcatcher
     systemctl start mailcatcher
 else
     echo "mailcatcher.service has already been configured."
 fi
 
-if [[ ! -f /etc/php/7.2/mods-available/mailcatcher.ini ]]; then
-    echo "Copying /srv/config/mailcatcher/mailcatcher.ini   /etc/php/7.2/mods-available/mailcatcher.ini"
+if [[ ! -f "/etc/php/7.2/mods-available/mailcatcher.ini" ]]; then
+    echo "copy /srv/config/mailcatcher/mailcatcher.ini   /etc/php/7.2/mods-available/mailcatcher.ini"
     cp "/srv/config/mailcatcher/mailcatcher.ini" "/etc/php/7.2/mods-available/mailcatcher.ini"
-    echo "Enable PHP Module for Mailcatcher"
+    echo "php module mailcatcher enabled"
     phpenmod mailcatcher
-    echo "Restarting Apache Server"
+    echo "restarting apache server"
     service apache2 restart
 fi
 
-if [[ ! -f "/usr/local/bin/composer" ]]; then
-    echo "installing composer"
-    EXPECTED_SIGNATURE="$(wget -q -O - https://composer.github.io/installer.sig)"
-    noroot php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-    ACTUAL_SIGNATURE="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
-
-    if [ "$EXPECTED_SIGNATURE" != "$ACTUAL_SIGNATURE" ]
-    then
-        >&2 echo 'ERROR: Invalid installer signature'
-        noroot rm composer-setup.php
-        exit 1
-    fi
-
-    noroot php composer-setup.php --quiet
-    noroot rm composer-setup.php
-    noroot chmod +x composer.phar
-    mv composer.phar /usr/local/bin/composer
-else
-    echo "composer is installed."
-fi
-
-# Installing WP-Cli
-if [[ ! -f "/usr/local/bin/wp" ]]; then
-    echo "downloading wp-cli"
-    noroot curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-    noroot chmod +x wp-cli.phar
-    mv wp-cli.phar /usr/local/bin/wp
-else
-    echo "wp-cli is installed."
-fi
-
-echo "Generating a root certificate for web development."
 if [[ ! -d "/vagrant/certificates/ca" ]]; then
-    noroot mkdir -p /vagrant/certificates/ca
-    noroot openssl genrsa -out /vagrant/certificates/ca/ca.key 4096
-    noroot openssl req -x509 -new -nodes -key /vagrant/certificates/ca/ca.key -sha256 -days 3650 -out /vagrant/certificates/ca/ca.crt -subj "/CN=Sandbox Internal CA"
+    noroot mkdir -p "/vagrant/certificates/ca"
+    noroot openssl genrsa -out "/vagrant/certificates/ca/ca.key" 4096
+    noroot openssl req -x509 -new -nodes -key "/vagrant/certificates/ca/ca.key" -sha256 -days 3650 -out "/vagrant/certificates/ca/ca.crt" -subj "/CN=Sandbox Internal CA"
     a2enmod ssl headers rewrite
 else
     echo "a root certificate of ca has been generated."
