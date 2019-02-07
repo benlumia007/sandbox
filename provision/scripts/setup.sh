f99bead6bb0b8301e34eb4e4da5b4ea4f2bf37dc
Benjamin Lu <benlumia007@gmail.com>
2019-02-07 13:15:46 -0800

    removed files since its in core box

--- provision/scripts/setup.sh
+++ provision/scripts/setup.sh
@@ -2,18 +2,7 @@
 
 export DEBIAN_FRONTEND=noninteractive
 
-# MySQL Configuration
-if [[ ! -f "/var/log/mysql/slow.log" ]]; then
-    echo "copy /srv/config/mysql/my.cnf   /etc/mysql/my.cnf"
-    cp "/srv/config/mysql/my.cnf" "/etc/mysql/my.cnf"
-else
-    echo "my.cnf has been copied and configured."
-fi
-
-if [[ ! -f "/etc/php/7.2/mods-available/php-custom.ini" ]]; then
-    echo "copy /srv/config/php/php-custom.ini   /etc/php/7.2/mods-available/php-custom.ini"
-    cp "/srv/config/php/php-custom.ini" "/etc/php/7.2/mods-available/php-custom.ini"
-    phpenmod php-custom
-else
-    echo "php-custom.ini has been configured and enabled."
+# This will run if db_restore is set to true. 
+if [[ -d /vagrant/database/backups ]]; then
+    /vagrant/config/bin/db_restores
 fi
