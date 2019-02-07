2c842afaad6bc7ac8d4fb2e61f51ea2a5adc33d9
Benjamin Lu <benlumia007@gmail.com>
2019-02-07 11:14:18 -0800

    removed certain stuff

--- provision/scripts/setup.sh
+++ provision/scripts/setup.sh
@@ -3,13 +3,9 @@
 export DEBIAN_FRONTEND=noninteractive
 
 # MySQL Configuration
-if [[ ! -f "/srv/log/mysql/slow.log" ]]; then
+if [[ ! -f "/var/log/mysql/slow.log" ]]; then
     echo "copy /srv/config/mysql/my.cnf   /etc/mysql/my.cnf"
     cp "/srv/config/mysql/my.cnf" "/etc/mysql/my.cnf"
-    echo "restarting mysql server"
-    service mysql restart
-    mkdir -p "/srv/log/mysql"
-    touch "/srv/log/mysql/slow.log"
 else
     echo "my.cnf has been copied and configured."
 fi
@@ -18,10 +14,6 @@ if [[ ! -f "/etc/php/7.2/mods-available/php-custom.ini" ]]; then
     echo "copy /srv/config/php/php-custom.ini   /etc/php/7.2/mods-available/php-custom.ini"
     cp "/srv/config/php/php-custom.ini" "/etc/php/7.2/mods-available/php-custom.ini"
     phpenmod php-custom
-    mkdir -p "/srv/log/php"
-    touch "/srv/log/php/php_errors.log"
-    echo "restarting apache server"
-    service apache2 restart
 else
     echo "php-custom.ini has been configured and enabled."
 fi
