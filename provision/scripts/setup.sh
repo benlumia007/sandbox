6d04d367548374883700b83af08e2ae328b5a9e8
Benjamin Lu <benlumia007@gmail.com>
2019-02-07 11:57:47 -0800

    add mailcatcher to box instead

--- provision/scripts/setup.sh
+++ provision/scripts/setup.sh
@@ -17,24 +17,3 @@ if [[ ! -f "/etc/php/7.2/mods-available/php-custom.ini" ]]; then
 else
     echo "php-custom.ini has been configured and enabled."
 fi
-
-if [[ ! -f "/lib/systemd/system/mailcatcher.service" ]]; then
-    echo "copy /srv/config/mailcatcher/mailcatcher.service   /lib/systemd/system/mailcatcher.service"
-    cp "/srv/config/mailcatcher/mailcatcher.service" "/lib/systemd/system/mailcatcher.service"
-    echo "starting mailcatcher at startup"
-    systemctl enable mailcatcher
-    systemctl start mailcatcher
-else
-    echo "mailcatcher.service has already been configured."
-fi
-
-if [[ ! -f "/etc/php/7.2/mods-available/mailcatcher.ini" ]]; then
-    echo "copy /srv/config/mailcatcher/mailcatcher.ini   /etc/php/7.2/mods-available/mailcatcher.ini"
-    cp "/srv/config/mailcatcher/mailcatcher.ini" "/etc/php/7.2/mods-available/mailcatcher.ini"
-    echo "php module mailcatcher enabled"
-    phpenmod mailcatcher
-    echo "restarting apache server"
-    service apache2 restart
-fi
-
-a2enmod ssl headers rewrite
