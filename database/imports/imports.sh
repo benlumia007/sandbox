656703ea4fc1afa2bce8bbbd23a8c57b852e93fa
Benjamin Lu <benlumia007@gmail.com>
2019-02-06 16:09:53 -0800

    allows u to import databases if does not exist

---
+++ database/imports/imports.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+sandbox_config=/vagrant/sandbox-custom.yml
+
+db_restores=`cat ${sandbox_config} | shyaml get-value options.db_restores 2> /dev/null`
+
+get_sites() {
+    local value=`cat ${sandbox_config} | shyaml keys sites 2> /dev/null`
+    echo ${value:-$@}
+}
+
+noroot() {
+    sudo -EH -u "vagrant" "$@";
+}
+
+if [[ $db_restores != "False" ]]; then
+    for domain in `get_sites`
+    do 
+        sql=*.sql
+        cd "/srv/database/backups"
+
+        for data in $sql
+        do
+            noroot mysql -u root -e "CREATE DATABASE IF NOT EXISTS $domain"
+            noroot mysql -u root -e "GRANT ALL PRIVILEGES ON $domain.* TO 'wp'@'localhost';"
+            noroot mysql -u root "$domain" < "$data"
+        done
+    done
+fi
