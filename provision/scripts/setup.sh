ee6b2b0e25d818e63ee570c0cb2cf354204b8467
Benjamin Lu <benlumia007@gmail.com>
2019-02-08 00:15:20 -0800

    updated with comment

--- provision/scripts/setup.sh
+++ provision/scripts/setup.sh
@@ -2,7 +2,11 @@
 
 export DEBIAN_FRONTEND=noninteractive
 
-# This will run if db_restore is set to true. 
+# /vagrant/database/backups
+#
+# database will be backup to this location when you vagrant halt or vagrant destroy, this
+# allows you to be able to restore database if you do  vagrant destroy and kept all existing
+# files and folders exactly the way it is or else it will fail, so be creative and be careful.
 if [[ -d /vagrant/database/backups ]]; then
     /vagrant/config/bin/db_restores
 fi
