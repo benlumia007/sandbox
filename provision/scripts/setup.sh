57143799aaccd8a13566b0ff4527f9618cc6a48d
Benjamin Lu <benlumia007@gmail.com>
2019-02-08 20:09:46 -0800

    add script to enable/disable xdebug quietly

--- provision/scripts/setup.sh
+++ provision/scripts/setup.sh
@@ -2,11 +2,15 @@
 
 export DEBIAN_FRONTEND=noninteractive
 
-# /vagrant/database/backups
+# /srv/database/backups
 #
 # database will be backup to this location when you vagrant halt or vagrant destroy, this
 # allows you to be able to restore database if you do  vagrant destroy and kept all existing
 # files and folders exactly the way it is or else it will fail, so be creative and be careful.
-if [[ -d /vagrant/database/backups ]]; then
-    /vagrant/config/bin/db_restores
+if [[ -d /srv/database/backups ]]; then
+    /srv/config/bin/db_restores
+fi
+
+if [[ -f "/etc/php/7.2/mods-available/xdebug.ini" ]]; then
+    /srv/config/bin/xdebug
 fi
