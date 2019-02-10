c4cece0bb81a687b38427ecad4d5a9b8a8df0307
Benjamin Lu <benlumia007@gmail.com>
2019-02-09 21:36:07 -0800

    add more specific comments

--- provision/scripts/utility.sh
+++ provision/scripts/utility.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-PROVISIONER="/vagrant/provision/resources/${1}/${2}/provision.sh"
-if [[ -f $PROVISIONER ]]; then
-    ${PROVISIONER}
+# provision.sh
+#
+# this is where the utitilies comes in to play, when a feature is enabled then
+# it find  specific core feature and use this to run  provision to install or 
+# update a feature.
+provisioner="/vagrant/provision/resources/${1}/${2}/provision.sh"
+if [[ -f $provisioner ]]; then
+    ${provisioner}
 fi
