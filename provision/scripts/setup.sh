a840acf4ba11998ecf85a62352309089176c3fe0
Benjamin Lu <benlumia007@gmail.com>
2019-02-09 21:33:55 -0800

    add more specific comments

--- provision/scripts/setup.sh
+++ provision/scripts/setup.sh
@@ -11,6 +11,11 @@ if [[ -d /srv/database/backups ]]; then
     /srv/config/bin/db_restores
 fi
 
+# xdebug
+#
+# when you run this script, while the xdebug is set to true, it will then enable xdebug, with
+# no configs, this is something that you as a user needs to be configure the way you want it
+# to work. xdebug is set to false by default.
 if [[ -f "/etc/php/7.2/mods-available/xdebug.ini" ]]; then
     /srv/config/bin/xdebug
 fi
