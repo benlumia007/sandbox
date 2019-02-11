4b19bd00f52a6efccf6c828e03e1f115b9693b69
Benjamin Lu <benlumia007@gmail.com>
2019-02-11 08:48:40 -0800

    remvoed certs

--- provision/scripts/dashboard.sh
+++ provision/scripts/dashboard.sh
@@ -16,15 +16,6 @@ noroot() {
 # this will install a dashboard specifically under the following directory so that it can be
 # served as a site. 
 if [[ ! -d ${dir} ]]; then
-  if [[ ! -d "/vagrant/certificates/dashboard" ]]; then
-      mkdir -p "/vagrant/certificates/dashboard"
-      cp "/srv/config/certificates/domain.ext" "/vagrant/certificates/dashboard/dashboard.ext"
-      sed -i -e "s/{{DOMAIN}}/dashboard/g" "/vagrant/certificates/dashboard/dashboard.ext"
-      noroot openssl genrsa -out "/vagrant/certificates/dashboard/dashboard.key" 4096
-      noroot openssl req -new -key "/vagrant/certificates/dashboard/dashboard.key" -out "/vagrant/certificates/dashboard/dashboard.csr" -subj "/CN=dashboard"
-      noroot openssl x509 -req -in "/vagrant/certificates/dashboard/dashboard.csr" -CA "/vagrant/certificates/ca/ca.crt" -CAkey "/vagrant/certificates/ca/ca.key" -CAcreateserial -out "/vagrant/certificates/dashboard/dashboard.crt" -days 3650 -sha256 -extfile "/vagrant/certificates/dashboard/dashboard.ext"
-  fi
-
   echo "Copying apache2.conf    /etc/apache2/sites-available/dashboard.conf"
   cp "/srv/config/apache/apache.conf" "/etc/apache2/sites-available/dashboard.conf"
   sed -i -e "s/{{DOMAIN}}/dashboard/g" "/etc/apache2/sites-available/dashboard.conf"
