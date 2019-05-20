f277f2d5e0e96e38dfbbf4304371b37ec7042f5c
Benjamin Lu <benlumia007@gmail.com>
2019-05-20 12:10:27 -0700

    fixed location

--- provision/scripts/utility.sh
+++ provision/scripts/utility.sh
@@ -5,7 +5,7 @@
 # this is where the utitilies comes in to play, when a feature is enabled then
 # it find  specific core feature and use this to run  provision to install or 
 # update a feature.
-provisioner="/vagrant/provision/resources/${1}/${2}/provision.sh"
+provisioner="/srv/provision/resources/${1}/${2}/provision.sh"
 if [[ -f $provisioner ]]; then
     ${provisioner}
 fi
