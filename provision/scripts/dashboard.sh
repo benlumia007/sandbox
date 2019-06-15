a73af2d6ded57785c342ef2ef2bd55b8cd03d31a
Benjamin Lu <benlumia007@gmail.com>
2019-06-14 22:23:48 -0700

    fixed to support nginx

--- provision/scripts/dashboard.sh
+++ provision/scripts/dashboard.sh
@@ -28,9 +28,9 @@ noroot() {
 # this will install a dashboard specifically under the following directory so that it can be
 # served as a site. 
 if [[ ! -d ${dir} ]]; then
-  echo "copying apache2.conf    /etc/apache2/sites-available/dashboard.conf"
-  cp "/srv/config/apache/apache.conf" "/etc/apache2/sites-available/dashboard.conf"
-  sed -i -e "s/{{DOMAIN}}/dashboard/g" "/etc/apache2/sites-available/dashboard.conf"
+  echo "copying nginx.conf    /etc/nginx/conf.d/dashboard.conf"
+  cp "/srv/config/nginx/nginx.conf" "/etc/nginx/conf.d/dashboard.conf"
+  sed -i -e "s/{{DOMAIN}}/dashboard/g" "/etc/nginx/conf.d/dashboard.conf"
   a2ensite "dashboard.conf" -q
 fi
 
