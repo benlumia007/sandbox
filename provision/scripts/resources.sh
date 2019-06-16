9acefdd3d10922a3b2ccd6e1632471cf43ca791b
Benjamin Lu <benlumia007@gmail.com>
2019-06-16 12:34:05 -0700

    check if false !=

--- provision/scripts/resources.sh
+++ provision/scripts/resources.sh
@@ -7,6 +7,14 @@
 name=$2
 dir="/srv/provision/resources/${name}"
 
+date=`cat /vagrant/provisioning_at`
+folder="/var/log/provision/${date}/resources/${2}"
+file="${folder}/${2}.log"
+mkdir -p ${folder}
+touch ${file}
+exec > >(tee -a "${file}" )
+exec 2> >(tee -a "${file}" >&2 )
+
 # noroot
 #
 # noroot allows provision scripts to be run as the default user "vagrant" rather than the root
@@ -19,16 +27,20 @@ noroot() {
 #
 # This will run and provision phpMyAdmin and TLS-CA for use. Please note that TLS-CA is recommended
 # and should be installed locally before accessing the dashboard or any sites will be generated.
-if [[ ! -d "${dir}" ]]; then
-    noroot mkdir -p "${dir}"
-    noroot cp "/srv/config/resources/${name}/provision" "${dir}"
-    ${dir}/provision
-else
-  if [[ -d "/srv/provision/resources/phpmyadmin" ]]; then
-      echo ""
-  fi
+if [[ false != ${name} ]]; then
+    if [[ ! -d "${dir}" ]]; then
+        noroot mkdir -p "${dir}"
+        noroot cp "/srv/config/resources/${name}/provision" "${dir}"
+        ${dir}/provision
+    else
+    if [[ -d "/srv/provision/resources/phpmyadmin" ]]; then
+        echo ""
+    fi
 
-  if [[ "/srv/provision/resources/tls-ca" ]]; then
-      ${dir}/provision
-  fi
+    if [[ "/srv/provision/resources/tls-ca" ]]; then
+        ${dir}/provision
+    fi
+    fi
 fi
+
+
