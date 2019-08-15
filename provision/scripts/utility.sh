3e5f90bd72bb524c865937e7b96e5f815c0a3826
Benjamin Lu <benlumia007@gmail.com>
2019-08-14 22:49:35 -0700

    removed provision logs

--- provision/scripts/utility.sh
+++ provision/scripts/utility.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 
-# /var/log/provision/${date}/resources/${2}/${2}.log
-#
-# This will generate a ${2}.log during provision, if it exists then it will generate
-# a log to tell if it is suceeded or failed.
-date=`cat /vagrant/provisioning_at`
-folder="/var/log/provision/${date}/resources/${2}"
-file="${folder}/${2}.log"
-mkdir -p ${folder}
-touch ${file}
-exec > >(tee -a "${file}" )
-exec 2> >(tee -a "${file}" >&2 )
-
-
 # provision.sh
 #
 # this is where the utitilies comes in to play, when a feature is enabled then
