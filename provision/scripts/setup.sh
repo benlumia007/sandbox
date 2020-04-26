e88c2abe4790706c63829d808f353765046a20db
Benjamin Lu <benlumia007@users.noreply.github.com>
2020-04-26 14:13:42 -0700

    update xdebug from php7.2 to php7.4-fpm

--- provision/scripts/setup.sh
+++ provision/scripts/setup.sh
@@ -13,6 +13,6 @@ noroot() {
 # when you run this script, while the xdebug is set to true, it will then enable xdebug, with
 # no configs, this is something that you as a user needs to be configure the way you want it
 # to work. xdebug is set to false by default.
-if [[ -f "/etc/php/7.2/mods-available/xdebug.ini" ]]; then
+if [[ -f "/etc/php/7.4/mods-available/xdebug.ini" ]]; then
     /srv/config/bin/xdebug
 fi
