5fade05aeb0fde17223f2a11a34457528d12ab8c
Benjamin Lu <benlumia007@users.noreply.github.com>
2021-04-21 16:06:47 -0700

    restore to the wrong area of the directory

--- provision/scripts/database.sh
+++ provision/scripts/database.sh
@@ -5,6 +5,6 @@
 # database will be backup to this location when you vagrant halt or vagrant destroy, this
 # allows you to be able to restore database if you do  vagrant destroy and kept all existing
 # files and folders exactly the way it is or else it will fail, so be creative and be careful.
-if [[ -d /srv/database/backups ]]; then
+if [[ -d /srv/database ]]; then
     /srv/config/bin/db_restores
 fi
